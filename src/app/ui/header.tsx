a134c8b03746f73495f306748e00e6062847472e
Muhammad Ibrahim <muhdibee@gmail.com>
2024-05-14 15:23:41 +0100

    Add prefeers-color-scheme.

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,4 +1,4 @@
-import darkMode from '../../../public/dark-mode.svg';
+import moon from '../../../public/moon.svg';
 import Image from 'next/image'
 
 export default function Header() {
@@ -6,13 +6,15 @@ export default function Header() {
     return (
         <div className='flex justify-between shadow px-6 py-6 bg-dark md:px-20 md:py-7'>
             <div>Where in the world?</div>
-            <div>
+            <div className='flex space-x-2'>
                 <Image
-                  src={darkMode}
+                  src={moon}
                   alt='dark_mode'
-                  width={108}
-                  height={22}
+                  width={20}
+                  height={20}
+                  className='mr-1'
                 />
+                Dark Mode
             </div>                     
         </div>
     )
