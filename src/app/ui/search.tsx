4612fd28a9a9ec2022d855d81acf33d32f2d35f6
Muhammad Ibrahim <muhdibee@gmail.com>
2024-06-14 12:17:07 +0100

    Style search component.

--- src/app/ui/search.tsx
+++ src/app/ui/search.tsx
@@ -1,12 +1,16 @@
+"use client";
 import Image from "next/image";
 import search from "../../../public/search.svg";
+import { GoSearch } from "react-icons/go";
+import styles from "./search.module.css";
 
 export default function Search() {
   return (
-    <div className="mx-3 my-3 flex shadow px-2 py-2 w-4/12">
-      <Image src={search} height={18} width={18} alt="Search icon" />
+    <div
+      className={`${styles.searchContainer} mx-3 my-3 flex shadow rounded px-6 py-2 w-4/12`}>
+      <GoSearch className="text-lg mt-1" />
       <input
-        className=" ml-3"
+        className={`ml-3 w-full`}
         type="text"
         placeholder="Search for a country..."
       />
