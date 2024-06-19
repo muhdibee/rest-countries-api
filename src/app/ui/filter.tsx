011e3db5dc55fb06fb24e06c934c46361fe5b3f3
Muhammad Ibrahim <muhdibee@gmail.com>
2024-06-19 16:39:15 +0100

    Style filter component.

--- src/app/ui/filter.tsx
+++ src/app/ui/filter.tsx
@@ -1,10 +1,25 @@
+"use client";
+import { useState } from "react";
 import styles from "./filter.module.css";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export default function Filter() {
+  const [filter, setFilter] = useState("none");
+
+  const pathname = usePathname();
+
+  const handleChange = (filter: string): void => {
+    console.log(filter);
+  };
+
   return (
     <span>
-      <select className={`${styles.select} px-4 py-2 rounded`}>
-        <option>Filter by Region</option>
+      <select
+        className={`${styles.filter} px-4 py-2 rounded`}
+        onChange={(e) => handleChange(e.target.value)}>
+        <option className="selected" selected disabled>
+          Filter by Region
+        </option>
         <option value="africa">Africa</option>
         <option value="americas">Americas</option>
         <option value="asia">Asia</option>
