1919c29494453abeafca2c325f6a6a988ad7c39a
Muhammad Ibrahim <muhdibee@gmail.com>
2024-06-20 17:54:24 +0100

    Implement search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,44 +8,86 @@ import styles from "@/app/home.module.css";
 import Search from "./ui/search";
 import Filter from "./ui/filter";
 import { useEffect, useState } from "react";
+import { BackButton } from "./ui/country/buttons";
 
 export default function Home() {
   const allCountries = fetchCountries();
   const [countries, setCountries] = useState(allCountries);
 
-  // useEffect(() => {}, []);
-
   // const pathname = usePathname();
   const searchParams = useSearchParams();
   const region = searchParams.get("region");
+  const country = searchParams.get("country");
+
+  useEffect(() => {
+    checkIfNoSearchAndFilter();
+    checkNoCountries();
+    checkSearchAvailable();
+    checkFilterAvailable();
+  }, [countries, country, region]);
+
+  const checkIfNoSearchAndFilter = () => {
+    if (region === null && country === null) {
+      setTimeout(() => {
+        setCountries(allCountries);
+      }, 500);
+    }
+  };
+
+  const checkNoCountries = () => {
+    if (countries.length < 1) {
+      return (
+        <main className={`${styles.main}`}>
+          <p>country not found</p>
+        </main>
+      );
+    }
+  };
+
+  const checkSearchAvailable = () => {
+    if (country !== null) {
+      setTimeout(() => {
+        const filterResult = allCountries.filter(
+          (countryObj) =>
+            countryObj.name.toLocaleLowerCase() === country.toLocaleLowerCase()
+        );
+        setCountries(filterResult);
+      }, 500);
+    }
+  };
+
+  const checkFilterAvailable = () => {
+    if (region !== null) {
+      setTimeout(() => {
+        const filterResult = allCountries.filter(
+          (country) =>
+            country.region.toLocaleLowerCase() === region.toLocaleLowerCase()
+        );
+        setCountries(filterResult);
+      }, 500);
+    }
+  };
 
   if (countries.length < 1) {
     return (
       <main className={`${styles.main}`}>
-        <p>countries not found</p>
+        <BackButton />
+        <b className="flex justify-center text-lg">country not found</b>
       </main>
     );
-  } else if (region !== null) {
-    setTimeout(() => {
-      const filterResult = allCountries.filter(
-        (country) =>
-          country.region.toLocaleLowerCase() === region.toLocaleLowerCase()
-      );
-      setCountries(filterResult);
-    }, 500);
+  } else {
+    return (
+      <main className={`${styles.main}`}>
+        <div className={`${styles.searchFilterContainer}`}>
+          <Search />
+          <Filter />
+        </div>
+        <section className={styles.cardsContainer}>
+          {countries.map((country: Country) => {
+            return <Card key={country.name} country={country} />;
+          })}
+        </section>
+      </main>
+    );
   }
-
-  return (
-    <main className={`${styles.main}`}>
-      <div className={`${styles.searchFilterContainer}`}>
-        <Search />
-        <Filter />
-      </div>
-      <section className={styles.cardsContainer}>
-        {countries.map((country: Country) => {
-          return <Card key={country.name} country={country} />;
-        })}
-      </section>
-    </main>
-  );
 }
--- src/app/ui/search.tsx
+++ src/app/ui/search.tsx
@@ -13,15 +13,17 @@ export default function Search() {
     console.log(searchText.toLocaleLowerCase());
   };
 
-  const handleSubmit = () => {
-    router.push(`?country=${searchText.toLocaleLowerCase()}`);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/?country=${searchText.toLocaleLowerCase()}`);
+    setSearchText("");
   };
 
   return (
     <div
       className={`${styles.searchContainer} my-3 flex px-6 py-2 w-full md:w-6/12 lg:w-4/12 shadow rounded`}>
       <MdSearch className="text-lg mt-1" />
-      <form onSubmit={() => handleSubmit()}>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <input
           className={`ml-3 w-full`}
           type="text"
